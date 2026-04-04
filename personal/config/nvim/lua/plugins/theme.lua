2cc1e477883d71b7db01f8e1a9b122181fefe751
Aaron Hankin <amhankin@gmail.com>
2026-04-04 11:02:35 -0600

    move to kanagawa theme

--- personal/config/nvim/lua/plugins/theme.lua
+++ personal/config/nvim/lua/plugins/theme.lua
@@ -1,23 +1,39 @@
+-- old theme, not bad
+--return {
+--  { -- You can easily change to a different colorscheme.
+--    -- Change the name of the colorscheme plugin below, and then
+--    -- change the command in the config to whatever the name of that colorscheme is.
+--    --
+--    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+--    'folke/tokyonight.nvim',
+--    priority = 1000, -- Make sure to load this before all the other start plugins.
+--    config = function()
+--      ---@diagnostic disable-next-line: missing-fields
+--      require('tokyonight').setup {
+--        styles = {
+--          comments = { italic = false }, -- Disable italics in comments
+--        },
+--      }
+--
+--      -- Load the colorscheme here.
+--      -- Like many other themes, this one has different styles, and you could load
+--      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--      vim.cmd.colorscheme 'lunaperche'
+--    end,
+--  },
+--}
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'lunaperche'
-    end,
-  },
+  "rebelot/kanagawa.nvim",
+  lazy = false,    -- Load immediately at startup
+  priority = 1000, -- Load before all other plugins
+  config = function()
+    -- Load the colorscheme
+    require('kanagawa').setup({
+      -- your configuration here (optional)
+      styles = {
+        comments = { italic = false }, -- Disable italics in comments
+      },
+    })
+    vim.cmd("colorscheme kanagawa-wave")
+  end,
 }
