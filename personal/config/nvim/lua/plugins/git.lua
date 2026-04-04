90fb68141da593ef629a40b9e4d1e577a8c25a72
Aaron Hankin <amhankin@gmail.com>
2026-04-04 09:19:43 -0600

    vscode like diffs with NeoGit + codediff

--- personal/config/nvim/lua/plugins/git.lua
+++ personal/config/nvim/lua/plugins/git.lua
@@ -1,15 +1,49 @@
 return {
-	-- Adds git related signs to the gutter, as well as utilities for managing changes
-	{
-	  'lewis6991/gitsigns.nvim',
-	  opts = {
-	    signs = {
-	      add = { text = '+' },
-	      change = { text = '~' },
-	      delete = { text = '_' },
-	      topdelete = { text = '‾' },
-	      changedelete = { text = '~' },
-	    },
-	  },
-	},
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --{
+  --  'lewis6991/gitsigns.nvim',
+  --  opts = {
+  --    signs = {
+  --      add = { text = '+' },
+  --      change = { text = '~' },
+  --      delete = { text = '_' },
+  --      topdelete = { text = '‾' },
+  --      changedelete = { text = '~' },
+  --    },
+  --  },
+  --},
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+
+      -- Only one of these is needed.
+      --"sindrets/diffview.nvim",   -- optional
+      "esmuellert/codediff.nvim", -- optional
+
+      -- For a custom log pager
+      "m00qek/baleia.nvim", -- optional
+
+      ---- Only one of these is needed.
+      --"nvim-telescope/telescope.nvim", -- optional
+      --"ibhagwan/fzf-lua",            -- optional
+      --"nvim-mini/mini.pick",         -- optional
+      --"folke/snacks.nvim",           -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    },
+  },
+  {
+    "esmuellert/codediff.nvim", -- optional
+    opts = {
+      explorer = {
+        view_mode = "tree",
+        flatten_dirs = "true",
+      }
+    },
+  }
+
 }
