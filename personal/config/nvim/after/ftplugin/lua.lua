d521679c45f7a4047da82c3c485af82fbcc60d7d
Aaron Hankin <amhankin@gmail.com>
2026-04-04 09:03:18 -0600

    set default tab size for lua

---
+++ personal/config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+vim.opt.shiftwidth = 2   -- Size of an indent
+vim.opt.tabstop = 2      -- Number of spaces tabs count for
+vim.opt.softtabstop = 2  -- Number of spaces a tab counts for while editing
+vim.opt.expandtab = true -- Use spaces instead of tabs
