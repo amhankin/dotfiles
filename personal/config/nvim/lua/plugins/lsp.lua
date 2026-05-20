9f884be01679bd13596bf005e1886da855a0d5d2
Aaron Hankin <amhankin@gmail.com>
2026-05-20 07:33:29 -0600

    basedpyright instead of pyright

--- personal/config/nvim/lua/plugins/lsp.lua
+++ personal/config/nvim/lua/plugins/lsp.lua
@@ -186,7 +186,7 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -237,7 +237,7 @@ return {
 
       require('mason-lspconfig').setup {
         ensure_installed = {
-          --'pyright',
+          --'basedpyright',
         },
         automatic_installation = false,
         handlers = {
